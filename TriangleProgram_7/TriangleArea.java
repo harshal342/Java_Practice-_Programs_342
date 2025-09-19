b1ba87079bdcf43a406aceee175ee7a92f2732e3
harshal342 <harshalwagh342@gmail.com>
2025-09-19 23:13:52 +0530

    Seventh Program

---
+++ TriangleProgram_7/TriangleArea.java
@@ -0,0 +1,17 @@
+package TriangleProgram_7;
+
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to calculate area of Triangle\n");
+        System.out.print("Enter your base in cms: ");
+        double base = input.nextDouble();
+        System.out.print("Now, enter your perpendicular height in cms: ");
+        double height = input.nextDouble();
+
+        double area = (base * height)/2;
+        System.out.println("Your area of triangle is: " +area + " cms2");
+    }
+}
