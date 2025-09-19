f4be16039d6108b3586a0d3c89b9355880142fbe
harshal342 <harshalwagh342@gmail.com>
2025-09-19 11:33:24 +0530

    Sixth Program

---
+++ PerimeterProgram_6/PerimeterRectangle.java
@@ -0,0 +1,17 @@
+package PerimeterProgram_6;
+
+import java.util.Scanner;
+
+public class PerimeterRectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your all sides of perimeter of rectangle: ");
+        int A = input.nextInt();
+        int B = input.nextInt();
+        int C = input.nextInt();
+        int D = input.nextInt();
+
+        int result = A + B + C + D;
+        System.out.println("\n The perimeter of rectangle is: " + result);
+    }
+}
